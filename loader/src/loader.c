18ada1e61aca52367987d610b405cbb6ac7bc162
Krishnan Winter <krishnanwinter1@gmail.com>
2022-11-10 14:12:54 +1100

    revert to lucy's changes

--- loader/src/loader.c
+++ loader/src/loader.c
@@ -143,18 +143,6 @@ putc(uint8_t ch)
 {
     *((volatile uint32_t *)(0x00FF000030)) = ch;
 }
-#elif defined(BOARD_rpi4b)
-#define UART_BASE 0xfe201000
-#define MU_IO 0x00
-#define MU_LSR 0x14
-#define MU_LSR_TXIDLE (1 << 6)
-
-static void
-putc(uint8_t ch)
-{
-    while (!(*UART_REG(MU_LSR) & MU_LSR_TXIDLE));
-    *UART_REG(MU_IO) = (ch & 0xff);
-}
 #else
 #error Board not defined
 #endif
